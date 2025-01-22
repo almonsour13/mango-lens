f30e7319bc07b09ec610a6b938b511fec842c559
almonsour13 <almonsoursalida@gmail.com>
2025-01-23 06:51:00 +0800

    remove config

--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -1,6 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://jxnvawcqgjycdwmjrtcg.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4bnZhd2NxZ2p5Y2R3bWpydGNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc1NTgyMDQsImV4cCI6MjA1MzEzNDIwNH0.SHxfjoc-ANsqCBDsHgyi_01WgqD83ltp9aKseEOTIWo'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? ''
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ??''
 export const supabase = createClient(supabaseUrl, supabaseKey)
