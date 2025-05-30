9c3114aeee5178184a43b8950e8cecf9371d0b6d
almonsour13 <almonsoursalida@gmail.com>
2025-05-30 10:25:42 +0800

    sadasd

--- src/constant/classes.ts
+++ src/constant/classes.ts
@@ -1,10 +1,7 @@
 export const classes = [
     "Anthracnose",
     "Bacterial Canker",
-    "Cutting Weevil",
     "Die Back",
     "Gall Midge",
     "Healthy",
-    "Powdery Mildew",
-    "Sooty Mould",
 ];
